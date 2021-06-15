5221f1588a7b197382902eb5dbb5688bd0ee29fc
Alex Tochetto <alextochetto@gmail.com>
2021-06-15 18:49:00 -0300

    change to parameters

--- src/Stock/Service/StockService/StockMonitorService.cs
+++ src/Stock/Service/StockService/StockMonitorService.cs
@@ -13,12 +13,10 @@ namespace Stock.Service.StockService
     public class StockMonitorService : IStockMonitorService
     {
         private readonly IGatewayServiceProvider _gatewayServiceProvider;
-        private readonly IConfigurationServiceProvider _configurationServiceProvider;
 
-        public StockMonitorService(IGatewayServiceProvider gatewayServiceProvider, IConfigurationServiceProvider configurationServiceProvider)
+        public StockMonitorService(IGatewayServiceProvider gatewayServiceProvider)
         {
             _gatewayServiceProvider = gatewayServiceProvider;
-            _configurationServiceProvider = configurationServiceProvider;
         }
 
         public async Task Monitor()
@@ -34,16 +32,22 @@ namespace Stock.Service.StockService
 
             foreach (Quote quote in quotes)
             {
-                string url = $"https://finance.yahoo.com/quote/{quote.TickUrl}?p={quote.TickUrl}";
+                //string url = $"https://finance.yahoo.com/quote/{quote.TickUrl}?p={quote.TickUrl}";
+                string url = _gatewayServiceProvider.Get<IConfigurationServiceProvider>().Get<string>("StockUrl");
+                url = string.Format(url, quote.TickUrl, quote.TickUrl);
                 HtmlDocument htmlDocument = await stockHtmlAgilityPackService.GetDocument(url);
                 if (htmlDocument is null)
                     break;
 
-                HtmlNodeCollection nodes = await stockHtmlAgilityPackService.GetNodes(htmlDocument, "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div/span[1]");
+                //HtmlNodeCollection nodes = await stockHtmlAgilityPackService.GetNodes(htmlDocument, "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div/span[1]");
+                string parameterValue = _gatewayServiceProvider.Get<IConfigurationServiceProvider>().Get<string>("StockValue");
+                HtmlNodeCollection nodes = await stockHtmlAgilityPackService.GetNodes(htmlDocument, parameterValue);
                 quote.UpdatedAt = DateTime.Now;
-                quote.Value = decimal.Parse(nodes[0].InnerText, new CultureInfo("pt-BR"));
+                quote.Value = decimal.Parse(nodes[0].InnerText, new CultureInfo("en-US"));
 
-                nodes = await stockHtmlAgilityPackService.GetNodes(htmlDocument, "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[2]/div[1]/div[1]/h1");
+                //nodes = await stockHtmlAgilityPackService.GetNodes(htmlDocument, "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[2]/div[1]/div[1]/h1");
+                string parameterCompany = _gatewayServiceProvider.Get<IConfigurationServiceProvider>().Get<string>("StockCompany");
+                nodes = await stockHtmlAgilityPackService.GetNodes(htmlDocument, parameterCompany);
                 quote.CompanyName = nodes[0].InnerText;
             }
         }
